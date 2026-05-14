83741490d572dd399f962500cdb44dc57bc8d2e9
Manasi Mandal <manasimandal0305@gmail.com>
2026-05-14 22:42:02 +0530

    First Commit

--- server/controllers/webHooks.js
+++ server/controllers/webHooks.js
@@ -34,7 +34,7 @@ export const clerkWebhooks=async (req,res)=>{
             case 'user.updated':{
                 const userData={
                     email:data.email_addresses[0]?.email_address || "",
-                    name:data.first_name+" "+data.last_name,
+                    name:data.first_name+" "+data.last_name, 
                     image:data.image_url,
                 }
                 await User.findByIdAndUpdate(data.id,userData)
